309d29a813710e3c4a3a5134f13cbefec51ce3e2
dsymbol <88138099+dsymbol@users.noreply.github.com>
2021-11-22 09:28:20 +0000

    Added "Drop your address!" flair support

--- main.py
+++ main.py
@@ -34,31 +34,34 @@ def load_db(file):
     return list
 
 def main():
-    account = api(reddit_client_id, reddit_client_secret, reddit_username, reddit_password)
+    account = api(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD)
     reddit = account.authorize()
     reddit.read_only = False
     subreddit = reddit.subreddit("NFTsMarketplace")
-    replied_to = load_db("commentdb.txt")
+    commented = load_db("commentdb.txt")
     keywords = ["wallet", "address"]
 
+    print(f"Logged in as {REDDIT_USERNAME}")
+    sleep(1)
+    print("[!] Starting bot...")
     while True:
-        print("[!] Starting bot...")
+        print("-")
         for post in subreddit.hot(limit=25):
-            if post not in replied_to and any(x in post.title.lower() for x in keywords):
-                replied_to.append(post)
+            if post not in commented and any(x in post.title.lower() for x in keywords) or post not in commented and keywords[1] in post.link_flair_text:
+                commented.append(post)
                 with open('commentdb.txt', 'a') as f:
-                    f.write(str(post) + "\n")
+                    f.write(f"{str(post)}\n")
                 if "sol" in post.title.lower():
-                    post.reply(soladdress)
+                    post.reply(SOL_ADDRESS)
                 else:
-                    post.reply(ethaddress)
+                    post.reply(ETH_ADDRESS)
                 post.upvote()
                 print(f'[+] {post.title}')
                 random_sleep = random.randint(300, 600)
                 to_mins = random_sleep / 60; to_mins = round(to_mins, 1)
-                print(f"[!] Sleeping for {str(to_mins)} minutes") 
+                print(f"[-] Sleeping for {str(to_mins)} minutes") 
                 sleep(random_sleep)        
-        print("[*] Commented on all posts matching keywords, sleeping for six hours.\n")
+        print("[*] Commented on all hot posts matching keywords, sleeping for six hours.")
         sleep(21600)       
 
 if __name__ == '__main__':
