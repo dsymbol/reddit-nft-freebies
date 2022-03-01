db6553aa867aa202e72162b4f56b63b606e084c3
dsymbol <88138099+dsymbol@users.noreply.github.com>
2022-03-01 20:42:47 +0000

    Fixed first time run error

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class api:
         self.password = password
         self.user_agent = api.uagent(10)
 
+    @staticmethod
     def uagent(length):
         letters = string.ascii_lowercase
         result_str = ''.join(random.choice(letters) for i in range(length))
@@ -44,24 +45,26 @@ class api:
 
 def load_file(file):
     try:
-        list = []
+        l = []
         with open(file, 'r') as f:
             for line in f:
-                list.append(line.rstrip())
-        return list
+                l.append(line.rstrip())
+        return l
     except FileNotFoundError:
         with open('comment.db', 'w') as f:
             pass
+        return []
 
 def get_nft():
     account = api(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD)
     reddit = account.authorize()
     account.authorized(reddit)
     reddit.read_only = False
-    subreddit = reddit.subreddit("NFTsMarketplace")
     commented = load_file("comment.db")
+    subreddit = reddit.subreddit("NFTsMarketplace")
     keywords = ["wallet", "address"]
     sleep(1)
+    
     while True:
         try:
             for post in subreddit.hot(limit=25):
@@ -70,20 +73,20 @@ def get_nft():
                     commented.append(post)
                     with open('comment.db', 'a') as f:
                         f.write(f"{str(post)}\n")
-                    post.reply(f'Ethereum: {ETH_ADDRESS}\n\nSolana: {SOL_ADDRESS}')
+                    post.reply(ETH_ADDRESS)
                     post.upvote()
-                    print(f'[{datetime.now().replace(microsecond=0)}] {post.title}')
+                    print(f'{post.title}')
                     rndm_sleep = random.randint(300, 600); to_mins = rndm_sleep / 60; to_mins = round(to_mins, 1)
-                    print(f"zZz for {str(to_mins)} minutes...", end='\r')
+                    print(f"zZz for {str(to_mins)} minutes")
                     sleep(rndm_sleep)
         except:
-            print("[?] Error occured, retrying.")
+            print("Error occured, retrying.")
             sleep(500)
-        print(f"[{datetime.now().replace(microsecond=0)}] zZz for 6.0 hours...")
-        sleep(21600)  
+        print("+")
+        print(f"[{datetime.now().replace(microsecond=0)}] zZz for 6 hours")
+        sleep(21600)
 
 def main():
-    print("[!] Starting bot...")
     get_nft()
 
 if __name__ == '__main__':
